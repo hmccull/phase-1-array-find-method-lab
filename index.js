1e0f419c9a3db2c662d734f617ca1cd9fd15fbd0
Hannah McCullough <hmac091@gmail.com>
2021-11-23 21:18:00 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+function superbowlWin(recordArray) {
+    for (const record of recordArray) {
+        if (record.result === 'W') {
+            return record.year;
+        }
+    }
+}
 
+  record.find(superbowlWin);
